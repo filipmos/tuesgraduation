813820810f3bbd5934e2c10382bedd6137e4d8eb
Deni <denislav13@ubuntu.(none)>
2014-01-17 02:05:23 +0200

    now can change diplomants in diploma_works

--- oop/app/models/diploma_work.rb
+++ oop/app/models/diploma_work.rb
@@ -7,7 +7,6 @@ class DiplomaWork < ActiveRecord::Base
 	validates_inclusion_of :covenanted, :in => [true,false]
 	validates_inclusion_of :approved, :in => [true,false]
 
-
 	def diploma_supervisor_must_be_different_from_reviewer_id
 		errors.add(:reviewer, "Reviewer must be different fron Diploma Supervisor") unless
        self.diploma_supervisor_id != self.reviewer_id
@@ -27,4 +26,19 @@ class DiplomaWork < ActiveRecord::Base
 			@diplomants
 	end
 
+	def set_diplomants(new_diplomants_ids)
+		@new_diplomants_ids = new_diplomants_ids
+		remove_old_diplomants
+		@new_diplomants_ids.each do |d|
+			@diplomant =  Student.find_by_id(d.to_i)
+			@diplomant.update_attribute(:diploma_work_id, self.id) 
+		end
+	end
+
+	def remove_old_diplomants
+		@old_diplomants = self.diplomanti
+		@old_diplomants.each do |d|
+			d.update_attribute(:diploma_work_id, nil)
+		end
+	end
 end
